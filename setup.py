9b8ef7e166fce1077bbee51d35c9df30987a6958
Vincent Shields <vincentshields@Vincents-MacBook-Pro.local>
2018-08-01 13:31:50 -0700

    version 1.0.0

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+
+setup(
+
+    name = 'Galaxy',
+
+    version = '1.0.0',
+
+    py_modules = ['galaxy'],
+
+    author = 'Vincent Shields',
+
+    author_email = 'vince.shields913@gmail.com',
+
+    url = 'https://github.com/vlshields/galaxy',
+
+    description = 'A way to print pretty regression tables',
+
+	)
