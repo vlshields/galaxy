5967437f708749a3cfed83433c14090af910f3c2
Vincent Shields <vincentshields@Vincents-MacBook-Pro.local>
2018-10-09 17:52:59 -0700

    test commit for new updates

--- galaxy.py
+++ galaxy.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import statsmodels.formula.api as sm
 
+
 """A way to print pretty regression tables in python. Useful for econometrics
 and social research."""
 
@@ -10,6 +11,100 @@ def get_data():
     data = pd.read_csv('test_data.csv')
     data['college'] = data['educ_cat'] > 3
     return data
+
+def appendix(reg1,reg2=None,reg3=None,names=['(1)','(2)','(3)']):
+    
+    df2 = pd.DataFrame()
+    
+    if not reg2 and not reg3:
+        appendix1 = [
+            
+            "{:.3f}".format(reg1.nobs),
+
+            "{:.3f}".format(reg1.rsquared),
+
+            "{:.3f}".format(reg1.rsquared_adj),
+
+            '{:.3f}{}'.format(reg1.fvalue,get_ending(reg1.f_pvalue))
+            ]
+
+        appendixnames = ['Observations', 'Rsquared', 'Adj. Rsquared','Fstat']
+
+        df2[names[0]] = pd.Series(appendix1)
+        df2.index = pd.Series(appendixnames)
+
+    if reg2 and not reg3:
+
+        appendix1 = [
+            
+            "{:.3f}".format(reg1.nobs),
+
+            "{:.3f}".format(reg1.rsquared),
+
+            "{:.3f}".format(reg1.rsquared_adj),
+
+            '{:.3f}{}'.format(reg1.fvalue,get_ending(reg1.f_pvalue))
+            ]
+
+        appendix2 = [
+            
+            "{:.3f}".format(reg2.nobs),
+
+            "{:.3f}".format(reg2.rsquared),
+
+            "{:.3f}".format(reg2.rsquared_adj),
+
+            '{:.3f}{}'.format(reg2.fvalue,get_ending(reg2.f_pvalue))
+            ]
+
+        appendixnames = ['Observations', 'Rsquared', 'Adj. Rsquared','Fstat']
+        df2[names[1]] = pd.Series(appendix2)
+        df2[names[0]] = pd.Series(appendix1)
+        df2.index = pd.Series(appendixnames)
+
+    if reg2 and reg3:
+
+        appendix1 = [
+            
+            "{:.3f}".format(reg1.nobs),
+
+            "{:.3f}".format(reg1.rsquared),
+
+            "{:.3f}".format(reg1.rsquared_adj),
+
+            '{:.3f}{}'.format(reg1.fvalue,get_ending(reg1.f_pvalue))
+            ]
+
+        appendix2 = [
+            
+            "{:.3f}".format(reg2.nobs),
+
+            "{:.3f}".format(reg2.rsquared),
+
+            "{:.3f}".format(reg2.rsquared_adj),
+
+            '{:.3f}{}'.format(reg2.fvalue,get_ending(reg2.f_pvalue))
+            ]
+        appendix3 = [
+            
+            "{:.3f}".format(reg3.nobs),
+
+            "{:.3f}".format(reg3.rsquared),
+
+            "{:.3f}".format(reg3.rsquared_adj),
+
+            '{:.3f}{}'.format(reg3.fvalue,get_ending(reg2.f_pvalue))
+            ]
+
+        appendixnames = ['Observations', 'Rsquared', 'Adj. Rsquared','Fstat']
+        df2[names[2]] = pd.Series(appendix3)
+        df2[names[1]] = pd.Series(appendix2)
+        df2[names[0]] = pd.Series(appendix1)
+        df2.index = pd.Series(appendixnames)
+
+        df2 = df2[[names[0],names[1],names[2]]]
+        return df2
+
 
 def get_ending(pvalue):
     
@@ -109,7 +204,7 @@ def bottom(reg1):
     print('{:<}: {:>27}{}'.format('F Stat',round(reg1.fvalue,3),get_ending(reg1.f_pvalue)))
     print('{:-^80}'.format(''))
 
-def Galaxy(reg1,reg2=None,reg3=None, table_name='Regression Table', names=None):
+def Galaxy(reg1,reg2=None,reg3=None, table_name='Regression Table', names=['(1)','(2)','(3)']):
 
     """a function to print out a nice-looking regression table"""
 
@@ -181,7 +276,61 @@ def Galaxy(reg1,reg2=None,reg3=None, table_name='Regression Table', names=None):
             reg1format(index,param,se)
         
         bottom(reg1)
-           
+
+def Galaxydf(reg1,reg2=None,reg3=None, names=['(1)','(2)','(3)']):
+
+    df = pd.DataFrame()
+    
+
+    reglist1 = [
+'{:.3f}{}({:.3f})'.format(param, get_ending(pvalue),se) for param,pvalue,se in zip(reg1.params,reg1.pvalues,reg1.bse)
+    ]
+    indexlist1 = [index for index in reg1.params.index]
+    
+  
+    if reg2:
+        reglist2 = [
+'{:.3f}{}({:.3f})'.format(param, get_ending(pvalue),se) for param,pvalue,se in zip(reg2.params,reg2.pvalues,reg2.bse)
+    ]
+
+        indexlist2 = indexlist1 + [index for index in reg2.params.index if index not in indexlist1]
+    
+    if reg3:
+        reglist3 = [
+'{:.3f}{}({:.3f})'.format(param, get_ending(pvalue),se) for param,pvalue,se in zip(reg3.params,reg3.pvalues,reg3.bse)
+    ]
+        indexlist3 = indexlist2 + [index for index in reg3.params.index if index not in indexlist2]
+
+    if not reg2 and not reg3:
+        
+        df[names[0]] = reglist1
+        df.index = indexlist1
+
+        return df
+
+    if reg2 and not reg3:
+        
+        df[names[0]] = reglist1
+        df[names[1]] = pd.Series(reglist2)
+        df.index = indexlist2
+
+        return df
+
+    if reg2 and reg3:
+
+        df[names[2]] = pd.Series(reglist3)
+        df[names[1]] = pd.Series(reglist2)
+        df[names[0]] = pd.Series(reglist1)
+        df.index = pd.Series(indexlist3)
+
+        df2 = appendix(reg1,reg2,reg3,names=names)
+        df = df[[names[0],names[1],names[2]]]
+        df = df.append(df2)
+
+        return df.fillna('')
+
+
+
 
 def main():
     
@@ -191,7 +340,8 @@ def main():
     reg2 = sm.ols('ppvt~momage + educ_cat', data = data).fit()
     reg3 = sm.ols('ppvt~momage + educ_cat + college + momage:college', data = data).fit()
 
-    Galaxy(reg1,reg2,table_name='Table 1',names=['reg1','reg2','reg3'])
+    print(Galaxydf(reg1,reg2,reg3,names=['reg1','reg2','reg3']))
+
 
 
 if __name__ == '__main__':
